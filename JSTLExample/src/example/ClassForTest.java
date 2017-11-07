3b3a26b0c49d666eb417247fcb0ed5f2e6398194
Lena-Elena-Lena <elena_raz84@mail.ru>
2017-11-08 00:43:57 +0300

    new methods

--- JSTLExample/src/example/ClassForTest.java
+++ JSTLExample/src/example/ClassForTest.java
@@ -3,8 +3,14 @@ package example;
 /**
  * Created by bn on 07.11.2017.
  */
-class ClassForTest {
+final class ClassForTest {
     public void printSmth(){
         System.out.print("Sth");
+    }
+    public void printSmth(String h){
+        System.out.print(h);
+    }
+    public void printSmth2(String h){
+        System.out.print(h);
     }
 }
